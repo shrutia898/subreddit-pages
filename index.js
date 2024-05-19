6135051bc5377998d713275019aaa4f8e60bc6e5
Shruti Atitkar <118574196+shrutia898@users.noreply.github.com>
2024-05-18 18:10:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
-
-app.get('/', (req, res) =>{
-    res.render('home.ejs')
-})
-
-
 app.get('/r/:subreddit', (req, res)=>{
     const { subreddit } = req.params;
     const data = redditData[subreddit]
